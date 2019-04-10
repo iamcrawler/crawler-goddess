b10a0bdd919156fcd4bb47348e057aae14948367
iamcrawler <iamcrawler@sina.com>
2019-04-10 17:09:19 +0800

    es太占用内存了，去掉es，以后有需要再将三个地方注释回来

--- src/main/java/cn/iamcrawler/crawlergoddess/elasticsearch/user/EsUserService.java
+++ src/main/java/cn/iamcrawler/crawlergoddess/elasticsearch/user/EsUserService.java
@@ -1,7 +1,7 @@
-package cn.iamcrawler.crawlergoddess.elasticsearch.user;
-
-/**
- * Created by liuliang on 2019/4/8.
- */
-public class EsUserService {
-}
+//package cn.iamcrawler.crawlergoddess.elasticsearch.user;
+//
+///**
+// * Created by liuliang on 2019/4/8.
+// */
+//public class EsUserService {
+//}
