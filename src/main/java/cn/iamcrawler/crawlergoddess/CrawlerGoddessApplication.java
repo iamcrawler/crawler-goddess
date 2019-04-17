eab15a9dbd136284ac9d204d67a85159dc83a8a4
iamcrawler <iamcrawler@sina.com>
2019-04-17 17:53:56 +0800

    升级mybatisPlus版本，并且添加代码生成器

--- src/main/java/cn/iamcrawler/crawlergoddess/CrawlerGoddessApplication.java
+++ src/main/java/cn/iamcrawler/crawlergoddess/CrawlerGoddessApplication.java
@@ -1,5 +1,6 @@
 package cn.iamcrawler.crawlergoddess;
 
+import com.codingapi.txlcn.tm.config.EnableTransactionManagerServer;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +10,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableTransactionManagerServer
 @MapperScan("cn.iamcrawler.crawlergoddess.mapper")
 public class CrawlerGoddessApplication {
 
